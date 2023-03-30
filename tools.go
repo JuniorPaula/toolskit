1979846d73cc263aa695937471549cd8c62b6d7f
Junior Paula <luke.junnior@icloud.com>
2023-03-30 18:36:09 -0300

    Create func to uploaded one files

--- tools.go
+++ tools.go
@@ -39,6 +39,20 @@ type UploadedFile struct {
 	FileSize     int64
 }
 
+func (t *Tools) UploadOneFile(r *http.Request, uploadDir string, rename ...bool) (*UploadedFile, error) {
+	renameFile := true
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+
+	files, err := t.UploadFiles(r, uploadDir, renameFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return files[0], nil
+}
+
 func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadedFile, error) {
 	renameFile := true
 	if len(rename) > 0 {
