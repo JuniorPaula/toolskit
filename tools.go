51239db075684d8f07681be22f9b938bd08f30cd
Junior Paula <luke.junnior@icloud.com>
2023-04-01 10:57:58 -0300

    Writen WriteJSON func

--- tools.go
+++ tools.go
@@ -202,6 +202,7 @@ type JSONResponse struct {
 	Data    interface{} `json:"data,omitempty"`
 }
 
+// ReadJSON tries to read the body of request and converts from json into a go data variable
 func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
 	maxBytes := 1024 * 1024 // one meg
 	if t.MaxJSONSize != 0 {
@@ -258,5 +259,27 @@ func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data interface{
 		return errors.New("body must contain only one JSON values")
 	}
 
+	return nil
+}
+
+// WriteJSON takes a response status code and arbitrary data and wrires json to the client
+func (t *Tools) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
 	return nil
 }
